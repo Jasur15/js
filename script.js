b2cbb3e8310294545498a4af26b324d5947cd8b5
test <test>
2022-11-12 23:22:49 +0500

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+var modal = prompt("Ismingizni kiriting")
+console.log("Mening ismim " +modal);
+
+var modal = +prompt("Yoshingizni kiriting")
+console.log("Mening yoshim " +modal +" da")
+
+var misol = prompt("12 + 5 =?")
+console.log("Misol 1: 12 + 5 = "+(12+5)+ " Sizning javobingiz " +misol);
+
+var misol = prompt("38 - 5 =?")
+console.log("Misol 1: 38 - 5 = "+(38-5)+ " Sizning javobingiz " +misol);
+
+var misol = prompt("4 * 8 =?")
+console.log("Misol 2: 4 * 8 = "+(4*8)+ " Sizning javobingiz " +misol);
+
+var misol = prompt("100 / 10 =?")
+console.log("Misol 3: 100 / 10 = "+(100/10)+ " Sizning javobingiz " +misol);
+
+var misol = prompt("20 % 4 =?")
+console.log("Misol 3: 20 % 4 = "+(20%4)+ " Sizning javobingiz " +misol);
+
+alert("Natijani ko'rish uchun konsolni oching")
